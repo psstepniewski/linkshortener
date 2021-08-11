20706dac38a828a2ad23d46ffae9b52e1313e369
Pawel Stepniewski <pawel@stepniewski.tech>
2021-08-11 15:10:42 +0200

    ShortLinkControllerSpec - GET /short-links/:id should return to OriginalLinkUrl

--- test/ShortLinkControllerSpec.scala
+++ test/ShortLinkControllerSpec.scala
@@ -6,11 +6,11 @@ import controllers.ShortLinkController
 import org.scalatest.GivenWhenThen
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.components.OneAppPerSuiteWithComponents
-import play.api.http.Status.OK
+import play.api.http.Status.{OK, SEE_OTHER}
 import play.api.libs.json.{JsSuccess, Json, Reads, Writes}
 import play.api.routing.Router
-import play.api.test.Helpers.{contentAsJson, contentType, route, status}
-import play.api.test.{FakeRequest, Injecting}
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{contentAsJson, contentType, header, route, status}
 import play.api.{BuiltInComponents, BuiltInComponentsFromContext, Configuration, NoHttpFiltersComponents}
 
 import scala.concurrent.duration._
@@ -26,17 +26,22 @@ class ShortLinkControllerSpec extends PlaySpec with OneAppPerSuiteWithComponents
 
     lazy val router: Router = Router.from({
       case POST(p"/api/v1/short_links") => shortLinkController.postShortLinks
+      case GET(p"/api/v1/short_links/$shortLinkId") => shortLinkController.getShortLink(shortLinkId)
     })
 
     override lazy val configuration: Configuration = Configuration(PersistenceTestKitPlugin.config).withFallback(context.initialConfiguration)
   }
 
+  val originalLinkUrl = "https://stepniewski.tech/blog/post/4-linkshortener-with-akka-concept/"
+  var createdShortLinkId: String = ""
+
   "ShortLinkController#postShortLinks" should {
+
     "return new short link" in  {
       Given("fake request with originalLinkUrl")
       val request = FakeRequest("POST", "/api/v1/short_links")
         .withHeaders("Content-Type" -> "application/json")
-        .withBody(Json.toJson(PostShortLinks.Request("https://stepniewski.tech/blog/post/4-linkshortener-with-akka-concept/")))
+        .withBody(Json.toJson(PostShortLinks.Request(originalLinkUrl)))
 
       When("postShortLinks is requested")
       val result = route(app, request).get
@@ -45,6 +50,25 @@ class ShortLinkControllerSpec extends PlaySpec with OneAppPerSuiteWithComponents
       status(result) must equal(OK)
       contentType(result) must contain("application/json")
       contentAsJson(result).validate[Response] mustBe a[JsSuccess[_]]
+
+      createdShortLinkId = contentAsJson(result).validate[Response].map(_.shortLinkId).get
+    }
+  }
+
+  "ShortLinkController#getShortLink" should {
+
+    "return originalLinkUrl for known shortLinkId" in {
+      Given("fake request with known shortLinkId")
+      val request = FakeRequest("GET", s"/api/v1/short_links/$createdShortLinkId")
+        .withHeaders("Content-Type" -> "application/json")
+        .withBody("")
+
+      When("getShortLink is requested")
+      val result = route(app, request).get
+
+      Then("response redirects (303 code) to OriginalLinkUrl")
+      status(result) must equal(SEE_OTHER)
+      header("Location", result) must contain(originalLinkUrl)
     }
   }
 }
